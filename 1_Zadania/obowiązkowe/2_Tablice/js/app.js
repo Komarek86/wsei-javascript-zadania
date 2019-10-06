e60191ea9e2c7141d2fd150c02e4f3d93a3bf6b4
Komarek86 <49593338+Komarek86@users.noreply.github.com>
2019-10-06 18:53:53 +0200

    Update app.js

--- 1_Zadania/obowiązkowe/2_Tablice/js/app.js
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -4,3 +4,11 @@ console.log(array[array.length-1]);
 for(var i=0; i<array.length; i++) {
     console.log(array[i]);
 }
+
+function printTable(array) {
+    for(var i=0; i<array.length; i++) {
+        console.log(array[i]);
+    }
+}
+
+printTable([1,2,3,4]);
