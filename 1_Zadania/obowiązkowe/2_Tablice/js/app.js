02da029fe2c1a551b3749c08cfa1123547716b43
Agnieszka Krupińska <48626047+agnieszkakrupinska@users.noreply.github.com>
2019-10-06 19:10:33 +0200

    Update app.js

--- 1_Zadania/obowiązkowe/2_Tablice/js/app.js
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -27,3 +27,46 @@ for(var i=0; i<array.length;i++)
 {
     console.log(array[i]);
 }
+
+//ZADANIE 3
+
+function printTable(array)
+{
+    for(var int i=0; i<array.length;i++){
+        console.log(array[i]);
+    }
+}
+
+printTable([1,2,3,4]);
+
+//ZADANIE 4
+
+function multiply(array)
+{
+    var multi=1;
+    for(var i=0;i<array.length;i++)
+    {
+       multi=multi*array[i];
+    }
+    return multi;
+}
+multiply([1,2,3,4,5,6,7]);
+
+//ZADANIE 5
+
+function getEvenAvarage(array)
+{
+    var sum = 0;
+    var counter = 0;
+    array.forEach(element =>{
+        if(element % 2===0)
+        {
+            sum=sum+element;
+            counter++;
+        }
+    });
+    if(counter===0) return null;
+    return sum/counter;
+}
+
+console.log(getEvenAvarage([1,2,3,4,5,6,7]));
