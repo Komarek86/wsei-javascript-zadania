7b44c9e76bf8d3eafdd4cb82a5cc60e27926acde
Agnieszka Krupińska <48626047+agnieszkakrupinska@users.noreply.github.com>
2019-10-13 16:35:19 +0200

    Update tasks 3, 4, 5

--- 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
+++ 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
@@ -13,3 +13,59 @@ var task2Array = [
     [5, 6, 7, 8],
     [9, 10, 11, 12]
 ];
+
+// ZADANIE 0
+
+function checkArray(array)
+{
+
+}
+
+var arr = [
+    [11, 12],
+    [42, 2],
+    [-4, -120],
+    [0, 0],
+    [1, 34],
+    ];
+
+// ZADANIE 1
+
+// ZADANIE 2
+
+// ZADANIE 3
+
+function print2DArray(array2D) {
+    array2D.forEach(element => {
+        element.forEach(elem => {
+           console.log(elem);
+        })
+    })
+}
+print2DArray(arr);
+
+// ZADANIE 4
+
+var new2DArray = [
+    [1,2,2,3],
+    [0,9,3,4],
+    [11,2,3,5]
+];
+print2DArray(new2DArray);
+
+// ZADANIE 5
+
+function create2DArray(rows, columns) {
+    var array = [];
+    var temp = [];
+    var x = 1;
+    for(var i = 0; i < rows; i++) {
+        for(var j = 0; j < columns; j++) {
+            temp.push(x++);
+        }
+        array.push(temp);
+        temp = [];
+    }
+    return array;
+}
+create2DArray(4,4); 
